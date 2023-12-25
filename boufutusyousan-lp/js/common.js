b0d1e9cbf0b28bfb91efcbe394101d01baec18d1
Cellmuller <kawabata_tomoya@cellmuller.com>
2023-12-25 19:13:50 +0900

    調整

--- boufutusyousan-lp/js/common.js
+++ boufutusyousan-lp/js/common.js
@@ -1,14 +1,19 @@
 document.addEventListener('scroll', function () {
-    var startElement = document.getElementById('start');
-    var endElement = document.getElementById('end');
-    var button = document.querySelector('.floating-button');
+    if (window.matchMedia('(max-width: 768px)').matches) {
+        var startElement = document.getElementById('start');
+        var endElement = document.getElementById('end');
+        var button = document.querySelector('.floating-button');
 
-    var startElementTop = startElement.getBoundingClientRect().top;
-    var endElementTop = endElement.getBoundingClientRect().top;
+        var startElementTop = startElement.getBoundingClientRect().top;
+        var endElementTop = endElement.getBoundingClientRect().top;
 
-    if (startElementTop < window.innerHeight && endElementTop > 0) {
-        button.style.display = 'block'; // フローティングボタンを表示
+        if (startElementTop < window.innerHeight && endElementTop > 0) {
+            button.style.display = 'block';
+        } else {
+            button.style.display = 'none';
+        }
     } else {
-        button.style.display = 'none'; // フローティングボタンを非表示
+        var button = document.querySelector('.floating-button');
+        button.style.display = 'none';
     }
 });
