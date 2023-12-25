5c31e59822900f935f202380387a78961e626b5f
Cellmuller <kawabata_tomoya@cellmuller.com>
2023-12-25 18:18:20 +0900

    調整

--- boufutusyousan-lp/js/common.js
+++ boufutusyousan-lp/js/common.js
@@ -1,33 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const targetElement = document.getElementById('lp-boufutusyousan-floating');
-    const addTriggerElement = document.querySelector('.trigger-add-class');
-    const removeTriggerElement = document.querySelector('.trigger-remove-class');
-    let lastScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    let classAdded = false; // クラスが追加されたかどうかを追跡するフラグ
+document.addEventListener('scroll', function () {
+    var startElement = document.getElementById('start');
+    var endElement = document.getElementById('end');
+    var button = document.querySelector('.floating-button');
 
-    function checkPosition() {
-        const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-        const scrollingUp = currentScrollPosition < lastScrollPosition;
-        lastScrollPosition = currentScrollPosition;
+    var startElementTop = startElement.getBoundingClientRect().top;
+    var endElementTop = endElement.getBoundingClientRect().top;
 
-        if (addTriggerElement) {
-            const addTriggerPosition = addTriggerElement.getBoundingClientRect().top + window.pageYOffset;
-            if (currentScrollPosition >= addTriggerPosition || (scrollingUp && classAdded)) {
-                targetElement.classList.add('lp-boufutusyousan-floating-active');
-                classAdded = true;
-            }
-        }
-
-        if (removeTriggerElement) {
-            const removeTriggerPosition = removeTriggerElement.getBoundingClientRect().top + window.pageYOffset;
-            if (currentScrollPosition >= removeTriggerPosition) {
-                targetElement.classList.remove('lp-boufutusyousan-floating-active');
-                classAdded = false;
-            } else if (scrollingUp && currentScrollPosition < removeTriggerPosition && classAdded) {
-                targetElement.classList.add('lp-boufutusyousan-floating-active');
-            }
-        }
+    if (startElementTop < window.innerHeight && endElementTop > 0) {
+        button.style.display = 'block'; // フローティングボタンを表示
+    } else {
+        button.style.display = 'none'; // フローティングボタンを非表示
     }
-
-    window.addEventListener('scroll', checkPosition);
 });
