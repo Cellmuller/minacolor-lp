ffccb1dd79b1a69b3f8e10792c316b9773edddb3
Cellmuller <kawabata_tomoya@cellmuller.com>
2023-12-26 19:35:30 +0900

    フローティング調整

--- boufutusyousan-lp/js/common.js
+++ boufutusyousan-lp/js/common.js
@@ -1,13 +1,31 @@
+// document.addEventListener('scroll', function () {
+//     if (window.matchMedia('(max-width: 768px)').matches) {
+//         var startElement = document.getElementById('start');
+//         var endElement = document.getElementById('end');
+//         var button = document.querySelector('.floating-button');
+//
+//         var startElementTop = startElement.getBoundingClientRect().top;
+//         var endElementTop = endElement.getBoundingClientRect().top;
+//
+//         if (startElementTop < window.innerHeight && endElementTop > 0) {
+//             button.style.display = 'block';
+//         } else {
+//             button.style.display = 'none';
+//         }
+//     } else {
+//         var button = document.querySelector('.floating-button');
+//         button.style.display = 'none';
+//     }
+// });
+
 document.addEventListener('scroll', function () {
     if (window.matchMedia('(max-width: 768px)').matches) {
         var startElement = document.getElementById('start');
-        var endElement = document.getElementById('end');
         var button = document.querySelector('.floating-button');
 
         var startElementTop = startElement.getBoundingClientRect().top;
-        var endElementTop = endElement.getBoundingClientRect().top;
 
-        if (startElementTop < window.innerHeight && endElementTop > 0) {
+        if (startElementTop < window.innerHeight) {
             button.style.display = 'block';
         } else {
             button.style.display = 'none';
